6b292868908cc3c3ba329f1cf3631cc407b114e3
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-06 16:09:46 +0100

    added the utility script7

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import "./../apis/getApi.js";
 import Button from "./../components/button";
 import Img_1 from "./../assets/pic_1.png";
 import Spinner from "./../components/spinner";
-
+import "./../inc/scripts/utilities";
 
 const Home = () =>{
 

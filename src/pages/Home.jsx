e221cfbb3fca76e7bb20864c8cc13125e88e6af4
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-06 07:08:26 +0100

    Added the preloader component, finished the home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./../apis/getApi.js";
-import Header from "./../components/header";
 import Button from "./../components/button";
 import Img_1 from "./../assets/pic_1.png";
 import Spinner from "./../components/spinner";
@@ -8,20 +7,28 @@ import Spinner from "./../components/spinner";
 
 const Home = () =>{
 
+    const customBtnStyle = {
+        "fontSize":"18px"
+   
+    }
+
     return (
         <React.Fragment>
             <Spinner/>
-           <div className="weather-preloader container-fluid">
+           <div className="weather-preloader container-fluid d-flex align-items-center flex-column">
 
                 <main className="my-5 preloader-weather-heading" >
                     <h2 className="text-center text-capitalize m-auto fw-bold">How's today weather?</h2>
                 </main>
 
 
-                <section className="m-auto text-center img-container" id="img-container">
+                <section className="m-auto text-center img-container my-md-4 my-3" id="img-container">
                     <img src={Img_1} className="img-fluid m-auto preloader-img" height="700" width="550"/>
                 </section>
 
+                <br/>
+
+                <Button text="today's weather" style={customBtnStyle} className=" brand-btn m-auto my-5 width-toggle"/>
            </div>
            
         </React.Fragment>

2b21949c1ba3dafc6482a6d95f967d2962394788
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-06 17:12:30 +0100

    Added the utility component

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,16 @@
+import React from "react";
 import FooterNav from "./footerNav";
+import UtilityComponent from "./utilityFooterComponet";
 
 
 const Footer = () =>{
     return (
+    <React.Fragment>
+        <UtilityComponent/>
         <footer className="container-fluid footer-nav-container">
             <FooterNav/>
         </footer>
+    </React.Fragment>
     )
 }
 
--- src/components/utilityFooterComponet.jsx
+++ src/components/utilityFooterComponet.jsx
@@ -4,8 +4,17 @@ const UtilityComponent = () =>{
     return (
         <React.Fragment>
             <section className="utility-component">
+                    <div className="flex">
+                        <h3>sign up or login</h3>
+                        <span>{"X"}</span>
+                    </div>
+                    <section className="jumbotron">
+                        <h4>i am a high jumbotron</h4>
+                    </section>
 
             </section>
         </React.Fragment>
     )
 }
+
+export default UtilityComponent;

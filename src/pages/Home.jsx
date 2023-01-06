185909493191cc2704290a8bcc2294a83633534b
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-06 05:50:05 +0100

    Started with the home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,26 @@ import React from "react";
 import "./../apis/getApi.js";
 import Header from "./../components/header";
 import Button from "./../components/button";
+import Img_1 from "./../assets/pic_1.png";
 
 
 const Home = () =>{
 
     return (
         <React.Fragment>
-            <h3 className='text-capitalize'>Welcome home</h3>
+           <div className="weather-preloader container-fluid">
 
-            <br/>
-            <Button text="Hello world" style={{"color":"red"}}/>
-            <Header/>
+                <main className="my-5 preloader-weather-heading" >
+                    <h2 className="text-center text-capitalize m-auto fw-bold">How's today weather?</h2>
+                </main>
+
+
+                <section className="m-auto text-center img-container" id="img-container">
+                    <img src={Img_1} className="img-fluid m-auto" height="600" width="500"/>
+                </section>
+
+           </div>
+           
         </React.Fragment>
     )
 }

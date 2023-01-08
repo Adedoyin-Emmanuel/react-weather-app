651d4310c439d073cbf15c52d45a854ed2058705
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-08 18:11:15 +0100

    Added the close utility component function

--- src/components/utilityFooterComponet.jsx
+++ src/components/utilityFooterComponet.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import jQuery from 'jquery';
 
 const UtilityComponent = () =>{
+    function closeUtilityComponent(){
+        jQuery(($)=>{
+            $.noConflict();
+
+            $(".utility-component").removeClass("add-utility-component-height");
+        })
+    }
     return (
         <React.Fragment>
             <section className="utility-component align-items-center justify-content-around m-auto width-toggle-3">
-                    <div className="utility-notch"></div>
+                    <div className="utility-notch" onClick={closeUtilityComponent}></div>
                     <div className="d-flex align-items-center justify-content-center text-center">
                         <p className=" m-auto text-center  text-capitalize ">search result not found!</p>
                       

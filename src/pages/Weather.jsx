d476d4054ca85afa6b8cadd68ff1c679c7a121f5
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-09 08:01:32 +0100

    Added the weather app menu styling

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -28,44 +28,56 @@ const WeatherApp = () =>{
                         <h3>Lagos, 9ja</h3>
                          <p className="date-time text-muted brand-small-text">Today, Jan 9 5:49AM</p>
                     </section>
-                    <div className="toggle-btn"></div> 
+                    <div className="toggle-btn">
+
+                        <svg  height={"20px"} width={"20px"} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M96 288H32c-17.67 0-32 14.33-32 32v64c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32v-64c0-17.67-14.33-32-32-32zm160 0h-64c-17.67 0-32 14.33-32 32v64c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32v-64c0-17.67-14.33-32-32-32zm160 0h-64c-17.67 0-32 14.33-32 32v64c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32v-64c0-17.67-14.33-32-32-32zM96 96H32c-17.67 0-32 14.33-32 32v64c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32v-64c0-17.67-14.33-32-32-32zm160 0h-64c-17.67 0-32 14.33-32 32v64c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32v-64c0-17.67-14.33-32-32-32zm160 0h-64c-17.67 0-32 14.33-32 32v64c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32v-64c0-17.67-14.33-32-32-32z"/></svg>
+
+                    </div> 
                 </section>
 
-                <section className="current-weather-icon">
-                    
 
-                    
-                    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px"
-                        width="100px" height="100px" viewBox="0 0 100 100" enableBackground="new 0 0 100 100" >
-                    <g>
-                        <path d="M36.154,74.275c0,1.865-1.514,3.377-3.379,3.377s-3.377-1.512-3.377-3.377s3.377-6.901,3.377-6.901
-                            S36.154,72.41,36.154,74.275z"/>
-                        <path d="M50.691,84.553c0,1.866-1.512,3.378-3.379,3.378c-1.865,0-3.377-1.512-3.377-3.378c0-1.865,3.377-6.901,3.377-6.901
-                            S50.691,82.688,50.691,84.553z"/>
-                        <path d="M64.494,81.03c0,1.865-1.512,3.377-3.377,3.377s-3.379-1.512-3.379-3.377s3.379-6.902,3.379-6.902
-                            S64.494,79.165,64.494,81.03z"/>
-                        <g>
-                            <path d="M49.328,67.374c-5.65,0-10.625-3.513-12.576-8.707c-1.832,0.896-3.84,1.364-5.885,1.364
-                                c-7.416,0-13.451-6.034-13.451-13.452s6.035-13.452,13.451-13.452c1.301,0,2.58,0.188,3.82,0.557
-                                c1.803-5.487,6.877-9.215,12.764-9.215c2.854,0,5.639,0.914,7.934,2.59c2.861-1.698,6.107-2.59,9.441-2.59
-                                c10.23,0,18.553,8.322,18.553,18.55c0,10.229-8.322,18.553-18.553,18.553c-1.365,0-2.746-0.161-4.119-0.477
-                                C58.25,64.989,53.971,67.374,49.328,67.374z M37.439,55.763c0.146,0,0.295,0.027,0.434,0.083c0.35,0.139,0.613,0.438,0.705,0.803
-                                c1.248,4.931,5.668,8.375,10.75,8.375c4.088,0,7.834-2.239,9.777-5.844c0.262-0.483,0.822-0.723,1.352-0.574
-                                c1.455,0.407,2.926,0.615,4.369,0.615c8.934,0,16.203-7.269,16.203-16.203c0-8.934-7.27-16.201-16.203-16.201
-                                c-3.158,0-6.227,0.918-8.871,2.655c-0.434,0.283-1.002,0.251-1.398-0.08c-1.99-1.66-4.514-2.575-7.105-2.575
-                                c-5.221,0-9.672,3.557-10.82,8.649c-0.076,0.336-0.297,0.623-0.604,0.783c-0.305,0.16-0.666,0.178-0.986,0.047
-                                c-1.336-0.544-2.74-0.819-4.174-0.819c-6.121,0-11.102,4.981-11.102,11.103c0,6.122,4.98,11.103,11.102,11.103
-                                c2.102,0,4.156-0.6,5.941-1.735C37,55.826,37.219,55.763,37.439,55.763z"/>
+                <section className="current-weather-icon">   
+                    <svg
+                        version="1.1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="64"
+                        height="64"
+                        viewBox="0 0 64 64">
+                        <defs>
+                            <filter id="blur" width="200%" height="200%">
+                                <feGaussianBlur in="SourceAlpha" stdDeviation="3"/>
+                                <feOffset dx="0" dy="4" result="offsetblur"/>
+                                <feComponentTransfer>
+                                    <feFuncA type="linear" slope="0.05"/>
+                                </feComponentTransfer>
+                                <feMerge> 
+                                    <feMergeNode/>
+                                    <feMergeNode in="SourceGraphic"/> 
+                                </feMerge>
+                            </filter>
+                        </defs>
+                        <g filter="url(#blur)" id="thunder">
+                            <g transform="translate(20,10)">
+                                <g className="am-weather-cloud-1">
+                                    <path d="M47.7,35.4     c0-4.6-3.7-8.2-8.2-8.2c-1,0-1.9,0.2-2.8,0.5c-0.3-3.4-3.1-6.2-6.6-6.2c-3.7,0-6.7,3-6.7,6.7c0,0.8,0.2,1.6,0.4,2.3     c-0.3-0.1-0.7-0.1-1-0.1c-3.7,0-6.7,3-6.7,6.7c0,3.6,2.9,6.6,6.5,6.7l17.2,0C44.2,43.3,47.7,39.8,47.7,35.4z" fill="#91C0F8" stroke="white" strokeLinejoin="round" strokeWidth="1.2" transform="translate(-10,-6), scale(0.6)" />
+                                </g>
+                                <g>
+                                    <path d="M47.7,35.4     c0-4.6-3.7-8.2-8.2-8.2c-1,0-1.9,0.2-2.8,0.5c-0.3-3.4-3.1-6.2-6.6-6.2c-3.7,0-6.7,3-6.7,6.7c0,0.8,0.2,1.6,0.4,2.3     c-0.3-0.1-0.7-0.1-1-0.1c-3.7,0-6.7,3-6.7,6.7c0,3.6,2.9,6.6,6.5,6.7l17.2,0C44.2,43.3,47.7,39.8,47.7,35.4z" fill="#57A0EE" stroke="#FFFFFF" strokeLinejoin="round" strokeWidth="1.2" transform="translate(-20,-11)" />
+                                </g>
+                                <g transform="translate(-9,28), scale(1.2)">
+                                    <polygon className="am-weather-stroke" fill="orange" stroke="white" strokeWidth="1" points="14.3,-2.9 20.5,-2.9 16.4,4.3 20.3,4.3 11.5,14.6 14.9,6.9 11.1,6.9" />
+                                </g>
+                            </g>
                         </g>
-                    </g>
                     </svg>
-                    <section className="d-flex m-auto align-items-center justify-content-center">
-
-                            <Button text="search weather"className="brand-btn m-auto my-5 width-toggle" onClick={show}/>
-
-                    </section>
-
+                   
                 </section>
+                <section className="d-flex align-items-center justify-content-center">
+
+<Button text="search weather"className="brand-btn m-auto my-5 width-toggle" onClick={show}/>
+
+</section>
+
                 <Footer/>
             </div>
 

0e628671b173a0a17d9c186c4738ef42bfa6a5c2
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-09 05:59:12 +0100

    Adding the main weather app design

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -18,11 +18,26 @@ const WeatherApp = () =>{
     return (
         <React.Fragment>
             <div className="container-fluid d-flex flex-column my-2">
-                <h2 className="fs-2 text-capitalize text-center my-3">Zedd-weather app</h2>
+                {/* <h2 className="fs-2 text-capitalize text-center my-3">Zedd-weather app</h2>
 
                 <p className="text-sm-center">Getting weather information comrade </p>
                 <Button text="search weather"className="brand-btn m-auto my-5 width-toggle" onClick={show}/>
-                <Footer/>
+                <Footer/> */}
+                <section className="app-header">
+                    <section className="city-locaton">
+                        <h3>Lagos, 9ja</h3>
+                         <p className="date-time text-muted brand-small-text">Today, Jan 9 5:49AM</p>
+                    </section>
+                    <div className="toggle-btn"></div> 
+                </section>
+
+                <section className="current-weather-icon">
+                    
+
+                    
+
+
+                </section>
 
             </div>
 

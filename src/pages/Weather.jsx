a010691256f2dd2c221676b6c5111597887b23ef
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-09 04:13:22 +0100

    Modified the footer nav typography

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -18,7 +18,7 @@ const WeatherApp = () =>{
             <div className="container-fluid d-flex flex-column my-2">
                 <h2 className="fs-2 text-capitalize text-center my-3">Zedd-weather app</h2>
 
-                <p>Getting weather information comrade </p>
+                <p className="text-sm-center">Getting weather information comrade </p>
                 <Button text="search weather"className="brand-btn m-auto my-5 width-toggle" onClick={show}/>
                 <Footer/>
 

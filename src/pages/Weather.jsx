81b6fdad4c5bf3a7eafb47b231ab04765c5d587f
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-10 00:20:42 +0100

    changed the city component font weight

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -19,14 +19,10 @@ const WeatherApp = () =>{
     return (
         <React.Fragment>
             <div className="container-fluid d-flex flex-column my-2">
-                {/* <h2 className="fs-2 text-capitalize text-center my-3">Zedd-weather app</h2>
-
-                <p className="text-sm-center">Getting weather information comrade </p>
-                <Button text="search weather"className="brand-btn m-auto my-5 width-toggle" onClick={show}/>
-                <Footer/> */}
+             
                 <section className="app-header">
                     <section className="city-locaton">
-                        <h3>Lagos, 9ja</h3>
+                        <h5 className="fw-bold fs-5">Lagos, 9ja</h5>
                          <p className="date-time text-muted brand-small-text">Today, Jan 9 5:49AM</p>
                     </section>
                     <div className="toggle-btn">

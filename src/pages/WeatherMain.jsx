9125f1b7db9fd1195e23595f59deb0423864a7f8
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-10 20:25:20 +0100

    Added the footer component to the weather main route

--- src/pages/WeatherMain.jsx
+++ src/pages/WeatherMain.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import Footer from "../components/footer";
 const WeatherMain = props =>{
     return (
         <React.Fragment>
             <section className="container-fluid">
                     what's popping
+                <Footer/>
             </section>
         </React.Fragment>
     )

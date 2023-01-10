b76571bb958f65c646a65b8a8362a96f233a13c6
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-10 18:05:09 +0100

    Added the weather app caurosel

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -20,7 +20,7 @@ const WeatherApp = () =>{
     }
     return (
         <React.Fragment>
-            <div className="container-fluid d-flex flex-column my-2 " style={{"overflowX":"hidden"}}>
+            <div className="container-fluid d-flex flex-column py-2 brand-secondary-color" style={{"overflowX":"hidden"}}>
              
                 <section className="app-header d-flex justify-content-between">
                     <section className="city-locaton">
@@ -116,18 +116,59 @@ const WeatherApp = () =>{
 
             </section>
 
-            <section className="future-weather-forecast">
+            <section className="future-weather-forecast my-4">
 
 
-  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    
-    </div>
+                <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
 
+
+                    
+                                
+                <div className="carousel-inner">
+                <div className="carousel-item active">
+                        <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+
+                        <div className="container">
+                        <div className="carousel-caption text-start">
+                            <h1>Example headline.</h1>
+                            <p>Some representative placeholder content for the first slide of the carousel.</p>
+                            <p><a className="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+                        </div>
+                        </div>
+                </div>
+
+                <div class="carousel-item">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+
+                        <div class="container">
+                        <div class="carousel-caption">
+                            <h1>Another example headline.</h1>
+                            <p>Some representative placeholder content for the second slide of the carousel.</p>
+                            <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+                        </div>
+                        </div>
+                        
+                </div>
+                <div class="carousel-item">
+                        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+
+                        <div class="container">
+                        <div class="carousel-caption">
+                            <h1>Another example headline.</h1>
+                            <p>Some representative placeholder content for the second slide of the carousel.</p>
+                            <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+                        </div>
+                        </div>
+                        
+                </div>
+                </div>
+                
+              </div>
             </section>
 
             <section className="d-flex align-items-center justify-content-center">

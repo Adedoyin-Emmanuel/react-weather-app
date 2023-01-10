0cece5af958bb0ef6f469e7da84498d6da8cd212
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-10 18:26:58 +0100

    Added the future weather component element

---
+++ src/components/futureWeatherComponent.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const futureWeatherComponent = props =>{
+   
+    return (
+       <React.Fragment>
+            <section className="future-weather-container d-flex align-items-center justify-content-center flex-column">
+                <section className="weather-wrapper">
+                    <p className="brand-text-small text-muted">{props.time}</p>
+                    <section className="weather-icon-section">
+                            {props.icon}
+                    </section>
+                    <p className="brand-text-small fw-bold"> {props.weatherUnit}<sup>o</sup></p>
+                </section>
+            </section> 
+       </React.Fragment>
+    )
+}
+
+
+export default futureWeatherComponent;

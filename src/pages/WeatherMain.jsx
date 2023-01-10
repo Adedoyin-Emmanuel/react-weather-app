c1ef61f468d9089264e764639383db7c5d758e50
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-11 00:05:08 +0100

    Modified the mainweather component

--- src/pages/WeatherMain.jsx
+++ src/pages/WeatherMain.jsx
@@ -3,7 +3,7 @@ import Footer from "../components/footer";
 const WeatherMain = props =>{
     return (
         <React.Fragment>
-            <section className="container-fluid">
+              <section className="container-fluid d-flex flex-column py-2 " style={{"overflowX":"hidden"}}>
                     <h3>what's popping</h3>
                 <Footer/>
             </section>

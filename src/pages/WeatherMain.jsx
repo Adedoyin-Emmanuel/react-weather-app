ab05c84bca1ab9065d8a641e68d1f22461d4123d
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-10 20:20:37 +0100

    Added the main weather page

---
+++ src/pages/WeatherMain.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const WeatherMain = props =>{
+    return (
+        <React.Fragment>
+            <section className="container-fluid">
+
+            </section>
+        </React.Fragment>
+    )
+}
+
+
+export default WeatherMain;

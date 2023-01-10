3b7020a53741f4dacf8a0048530a7f2708194c7d
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-10 20:14:25 +0100

    Added the footer component

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Footer from "../components/footer";
 const Settings = () =>{
     return (
         <React.Fragment>
             <section className="container-fluid">
                     <Link to={"/Weather"}>home</Link>
                     <h3 className="text-center">change settings</h3>
+                    <Footer/>
             </section>
         </React.Fragment>
     )
--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 const Support = () =>{
     return (
         <React.Fragment>

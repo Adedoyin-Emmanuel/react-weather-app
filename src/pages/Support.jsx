035d90a52e6b3d6f8da66551d039daf6e3f2892b
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-10 20:13:01 +0100

    Added the home route

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Support = () =>{
     return (
         <React.Fragment>
-            <section className="container-fluid">
+            <section className="container-fluid my-3">
+                    <Link to="/weather">Home</Link>
                     <h4 className="text-center text-capitalize">abeg support your guy!</h4>
             </section>
         </React.Fragment>

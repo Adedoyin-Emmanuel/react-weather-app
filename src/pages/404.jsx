a035f47bc9a6fee482d637301a96b17371edf240
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-10 20:32:47 +0100

    Added the button component

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-
+import Button from "../components/button";
 const NotFound = ()=>{
+    
+
     return (
         <React.Fragment>
             <section className="container-fluid">
-                    <h2 className="text-capitalize my-3">not found</h2>
+                    <h2 className="text-capitalize my-3 fs-3 fw-bold">not found!</h2>
 
                     <p className="text-muted text-capitalize">the page requested for was not found!</p>
+
+                    <Button text="search weather"className="brand-btn m-auto my-5 width-toggle" onClick={returnHome}/>
+
             </section>
         </React.Fragment>
     )

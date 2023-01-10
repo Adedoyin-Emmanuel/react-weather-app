2363a7e7effe57113cdf69ffe4b5fb9bf2d197f7
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-10 18:17:17 +0100

    roll back

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -119,56 +119,6 @@ const WeatherApp = () =>{
             <section className="future-weather-forecast my-4">
 
 
-                <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
-                    <div className="carousel-indicators">
-                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1">Today</button>
-                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2">tomorrow</button>
-                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3">10 days</button>
-                    </div>
-
-
-                    
-                                
-                <div className="carousel-inner">
-                <div className="carousel-item active">
-                        <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
-
-                        <div className="container brand-secondary-color">
-                        <div className="carousel-caption text-start">
-                            <p>Example headline.</p>
-                            <p>Some representative placeholder content for the first slide of the carousel.</p>
-                            <p><a className="btn btn-lg btn-primary" href="#">Sign up today</a></p>
-                        </div>
-                        </div>
-                </div>
-
-                <div className="carousel-item">
-                        <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
-
-                        <div className="container">
-                        <div className="carousel-caption">
-                            <h1>Another example headline.</h1>
-                            <p>Some representative placeholder content for the second slide of the carousel.</p>
-                            <p><a className="btn btn-lg btn-primary" href="#">Learn more</a></p>
-                        </div>
-                        </div>
-                        
-                </div>
-                <div className="carousel-item">
-                        <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
-
-                        <div className="container">
-                        <div className="carousel-caption">
-                            <h1>Another example headline.</h1>
-                            <p>Some representative placeholder content for the second slide of the carousel.</p>
-                            <p><a className="btn btn-lg btn-primary" href="#">Learn more</a></p>
-                        </div>
-                        </div>
-                        
-                </div>
-                </div>
-                
-              </div>
             </section>
 
             <section className="d-flex align-items-center justify-content-center">

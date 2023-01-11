6ba1a0748b64a9136dcefe57b5715c39c28e3f6e
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-11 16:07:07 +0100

    Added the navigate home function

--- src/pages/WeatherMain.jsx
+++ src/pages/WeatherMain.jsx
@@ -3,9 +3,13 @@ import rainIcon from "./../assets/rain (1).png";
 import windIcon from "./../assets/windy.png";
 import humidity from "./../assets/humidity.png";
 import Footer from "../components/footer";
+import navigate from "../inc/scripts/utilities";
 
 const WeatherMain = (props) => {
 
+	const navigateHome = () =>{
+		navigate("/");
+	}
     
 	return (
 		<React.Fragment>
@@ -14,14 +18,8 @@ const WeatherMain = (props) => {
 				style={{ overflowX: "hidden" }}>
 				<section className="app-header d-flex justify-content-between">
 					<div className="toggle-btn ">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width={"24px"}
-							height={"24px"}
-							viewBox="0 0 24 24">
-							<path fill="none" d="M0 0h24v24H0V0z" />
-							<path d="M4 13h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1zm0 8h6c.55 0 1-.45 1-1v-4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1zm10 0h6c.55 0 1-.45 1-1v-8c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1zM13 4v4c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1z" />
-						</svg>
+					<svg height={"30"} id="Layer_1" version="1.1" onClick={navigateHome}viewBox="0 0 512 512" width={"30"} xmlns="http://www.w3.org/2000/svg" ><polygon points="352,128.4 319.7,96 160,256 160,256 160,256 319.7,416 352,383.6 224.7,256 "/></svg>
+						
 					</div>
 					<section className="city-locaton">
 						<h5 className="fw-bold fs-5">Lagos, 9ja</h5>

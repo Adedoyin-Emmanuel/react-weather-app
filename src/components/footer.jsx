7836e1813b83c7bc4a086ef438f447d36c3a586e
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-11 05:04:28 +0100

    Modified the .gitignore

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,19 +3,20 @@ import FooterNav from "./footerNav";
 import UtilityComponent from "./utilityFooterComponet";
 
 const Footer = () => {
-	const customFooterStyle = {
-		zIndex: "10",
-	};
-	return (
-		<div
-			className="m-auto d-flex align-items-center justify-content-center d-md-none"
-			style={customFooterStyle}>
-			<UtilityComponent />
-			<footer className="shadow-lg d-flex align-items-center justify-content-center footer-nav-container">
-				<FooterNav />
-			</footer>
-		</div>
-	);
+  const customFooterStyle = {
+    zIndex: "10",
+  };
+  return (
+    <div
+      className="m-auto d-flex align-items-center justify-content-center d-md-none"
+      style={customFooterStyle}
+    >
+      <UtilityComponent />
+      <footer className="shadow-lg d-flex align-items-center justify-content-center footer-nav-container">
+        <FooterNav />
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;

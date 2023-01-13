3f0930a4b9de1a102938ad82b1134a8762890814
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-13 13:34:57 +0100

    Added the settings page

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Settings = () =>{
+    return (
+        <React.Fragment>
+            <section className="container-fluid">
+                    change settings!
+            </section>
+        </React.Fragment>
+    )
+}
+
+export default Settings;

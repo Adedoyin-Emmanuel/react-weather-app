7092ca5d2c2dfa30f7189eda1de919b6e214f747
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-13 13:35:45 +0100

    Added the settings route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import WeatherApp from "./pages/Weather";
 import WeatherMain from "./pages/WeatherMain";
 import NotFound from "./pages/404";
 import ForecastWeather from "./pages/ForecastWeather";
+import Settings from "./pages/Settings";
 import "./autoload";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="weather" element={<WeatherApp />} />
         <Route path="weathermain" element={<WeatherMain />} />
         <Route path="forecast" element={<ForecastWeather />} />
+        <Route path="settings" element={<Settings/>}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>

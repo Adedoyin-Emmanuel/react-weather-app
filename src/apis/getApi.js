184a504d2a25d77e436b8e587eb8b5e3d716fae2
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-13 10:20:29 +0100

    Bug fix

--- src/apis/getApi.js
+++ src/apis/getApi.js
@@ -5,17 +5,6 @@ import jQuery from "jquery";
         $.noConflict();
     
        $.ajax({
-            url: "https://jsonplaceholder.typicode.com/users/",
-            success: (results) =>{
-                //console.log(results[0]);
-                
-                results.forEach((result,index)=>{
-                    console.log(result);
-                });
-                
-            },
-            error: (xhr,status,error)=>{
-                console.log(status);
-            }
+            
        })
     });

8c5443f1b91b25985f2dd10bc5a968b40ee562d8
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-14 22:28:53 +0100

    Bug fix

--- src/components/forecastWeatherItems.jsx
+++ src/components/forecastWeatherItems.jsx
@@ -6,11 +6,11 @@ const ForecastWeatherItems = props =>{
        <React.Fragment>
             <section className="future-weather-container d-flex align-items-center justify-content-between flex-column brand-bg-white px-4  rounded-3 shadow mx-4">
                 <section className="weather-wrapper d-flex flex-column align-items-center justify-content-between">
-                    <p className="brand-small-text text-dark py-1 text-center">{props.name}</p>
+                    <p className="brand-small-text-2 text-dark py-1 text-center m-0">{props.name}</p>
                     <section className="weather-icon-section py-1">
                             <img src ={props.icon} height={"30"} width={"30"} alt={"weather-image"}/>
                     </section>
-                    <p className="brand-small-text-2 fw-bold text-center text-dark"> {props.weatherUnit}<sup>o</sup></p>
+                    <p className="brand-small-text fw-bold text-center text-dark m-0"> {props.weatherUnit}<sup>o</sup></p>
                 </section>
             </section> 
        </React.Fragment>

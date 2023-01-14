c652b9ca5861d17d564a1f156fdccdfeca699adf
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-14 15:40:00 +0100

    Added the restoreFactorySettings function

--- src/backend/settings.js
+++ src/backend/settings.js
@@ -1,6 +1,7 @@
 import { db } from "../backend/app_backend";
 import jQuery from "jquery";
 import Swal from "sweetalert2";
+import navigate from "../inc/scripts/utilities";
 
 export const saveLocation = (e) => {
   e.preventDefault();
@@ -40,5 +41,6 @@ export const getDefaultLocation = () => {
 };
 
 export const restoreFactorySettings = ()=>{
-    
+    db.destroy();
+    navigate("/");
 }

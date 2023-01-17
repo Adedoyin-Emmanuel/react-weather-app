5ad411ceae1980a05be9c36beb92a149b7375045
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-17 14:06:37 +0100

    connected to the API

--- src/apis/getApi.js
+++ src/apis/getApi.js
@@ -1,9 +1,18 @@
 import jQuery from "jquery";
 
+const API_KEY = "cd34f692e856e493bd936095b256b337";
 export const getCurrentWeather = () => {
   jQuery(($) => {
     let userSearch = $("#searchWeather").val();
 
-    console.log(userSearch);
+    const SEARCH_URL = `https://api.openweathermap.org/data/2.5/weather?q=${userSearch}&appid=${API_KEY}`
+
+    $.ajax({
+      url:SEARCH_URL,
+      processData:false,
+      success: (result)=>{
+        console.log(result);
+      }
+    })
   });
 };

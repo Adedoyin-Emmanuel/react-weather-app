999902c89abf8018b6c41ad2a415409a22ab3b9b
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-20 07:04:32 +0100

    Added the scroll to top for the error alert

--- src/apis/getGeolocation.js
+++ src/apis/getGeolocation.js
@@ -12,66 +12,68 @@ const getGeolocation = () => {
 		};
 		const error = (error) => {
 			Swal.fire({
-                toast:true,
-                text:error.message,
-                icon:"warning",
-                timer:3000,
-                position:"top",
-                showConfirmButton:false
-            })
+				toast: true,
+				text: error.message,
+				icon: "warning",
+				timer: 3000,
+				position: "top",
+				showConfirmButton: false,
+			});
 		};
 		navigator.geolocation.getCurrentPosition(
 			(position) => {
 				//check if the user's position was saved before
-               if(!db.get("USER_LONGITUDE") && !db.get("USER_LATITUDE")){
-                    db.create("USER_LONGITUDE",position.coords.longitude);
-                    db.create("USER_LATITUDE",position.coords.latitude);
-               }else{
-                //if saved, then get the current weather using their coordinates
-                
-                jQuery(($)=>{
-                    $.noConflict();
+				if (!db.get("USER_LONGITUDE") && !db.get("USER_LATITUDE")) {
+					db.create("USER_LONGITUDE", position.coords.longitude);
+					db.create("USER_LATITUDE", position.coords.latitude);
+				} else {
+					//if saved, then get the current weather using their coordinates
 
-                    const longitude = db.get("USER_LONGITUDE"), latitude = db.get("USER_LATITUDE");
-                    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weatherAPI.API_KEY}&units=metric`;
+					jQuery(($) => {
+						$.noConflict();
 
-					$.ajax({
-						url:URL,
-						processData:false,
-						success:(result,status,xhr)=>{
-							if(xhr.status != 200){
-								Swal.fire({
-									toast:true,
-									position:"top",
-									text:"Something went wrong!",
-									icon:"info",
-									showConfirmButton:false,
-									timer:3000,
-			
-								});
-							}else{
-								//if API call was successful
-								if(result.cod == 200){
-									weatherAPI.updateReactDom(result);
-									weatherAPI.scrollToElement("weatherContainer");
+						const longitude = db.get("USER_LONGITUDE"),
+							latitude = db.get("USER_LATITUDE");
+						const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weatherAPI.API_KEY}&units=metric`;
+
+						$.ajax({
+							url: URL,
+							processData: false,
+							success: (result, status, xhr) => {
+								if (xhr.status != 200) {
+									Swal.fire({
+										toast: true,
+										position: "top",
+										text: "Something went wrong!",
+										icon: "info",
+										showConfirmButton: false,
+										timer: 3000,
+									}).then((willProceed)=>{
+										//scroll to top after notification
+										weatherAPI.scrollToElement("weatherContainer");
+
+									})
+								} else {
+									//if API call was successful
+									if (result.cod == 200) {
+										weatherAPI.updateReactDom(result);
+										weatherAPI.scrollToElement("weatherContainer");
+									}
 								}
-							}
-						},
-						error:(xhr,status,error)=>{
-							Swal.fire({
-								toast: true,
-								text: error,
-								icon: "warning",
-								timer: 3000,
-								position: "top",
-								showConfirmButton: false,
-							});
-						}
-					})
-                })
-
-
-               }
+							},
+							error: (xhr, status, error) => {
+								Swal.fire({
+									toast: true,
+									text: error,
+									icon: "warning",
+									timer: 3000,
+									position: "top",
+									showConfirmButton: false,
+								});
+							},
+						});
+					});
+				}
 			},
 			error,
 			OPTIONS

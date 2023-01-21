82849fdcfb75d402c54cbaed1b1f1219430a346b
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-21 05:44:39 +0100

    Added the save unit button component

--- src/backend/settings.js
+++ src/backend/settings.js
@@ -4,99 +4,96 @@ import Swal from "sweetalert2";
 import navigate from "../inc/scripts/utilities";
 
 export const saveLocation = (e) => {
-  e.preventDefault();
+	e.preventDefault();
 
-  jQuery(($) => {
-    $.noConflict();
+	jQuery(($) => {
+		$.noConflict();
 
-    const $defaultLocation = $("#defaultLocation").val().trim();
+		const $defaultLocation = $("#defaultLocation").val().trim();
 
-    //check if the location is empty
-    if ($defaultLocation === undefined || $defaultLocation == "") {
-      Swal.fire({
-        title: "Invalid Location!",
-        html: "<p class=' text-center text-danger'>Please enter a valid location</p>",
-        confirmButtonColor: "rgb(83, 166, 250)",
-        allowOutsideClick: false,
-        allowEscapeKey: false,
-        allowEnterKey: false,
-        timer: 4000,
-      });
-    } else {
-      db.update("USER_DEFAULT_LOCATION", $defaultLocation);
-      Swal.fire({
-        text: "Location updated successfully!",
-        icon: "success",
-        toast: true,
-        position: "top",
-        showConfirmButton: false,
-        timer: 3000,
-      });
-    }
-  });
+		//check if the location is empty
+		if ($defaultLocation === undefined || $defaultLocation == "") {
+			Swal.fire({
+				title: "Invalid Location!",
+				html: "<p class=' text-center text-danger'>Please enter a valid location</p>",
+				confirmButtonColor: "rgb(83, 166, 250)",
+				allowOutsideClick: false,
+				allowEscapeKey: false,
+				allowEnterKey: false,
+				timer: 4000,
+			});
+		} else {
+			db.update("USER_DEFAULT_LOCATION", $defaultLocation);
+			Swal.fire({
+				text: "Location updated successfully!",
+				icon: "success",
+				toast: true,
+				position: "top",
+				showConfirmButton: false,
+				timer: 3000,
+			});
+		}
+	});
 };
 
 export const getDefaultLocation = () => {
-  return db.get("USER_DEFAULT_LOCATION");
+	return db.get("USER_DEFAULT_LOCATION");
 };
 
 export const restoreFactorySettings = () => {
-  db.destroy();
-  navigate("/");
+	db.destroy();
+	navigate("/");
 };
 
 export const trackSavedLocationWeather = () => {
-  jQuery(($) => {
-    $.noConflict();
-    const $toggleBtn = document.getElementById("flexSwitchCheckDefault");
+	jQuery(($) => {
+		$.noConflict();
+		const $toggleBtn = document.getElementById("flexSwitchCheckDefault");
 
-    if ($toggleBtn.checked) {
-      //check if the value is in the database, then update it
-      if (db.get("TRACK_SAVED_LOCATION_WEATHER")) {
-        db.update("TRACK_SAVED_LOCATION_WEATHER", true);
-        Swal.fire({
-          text: "Saved location would be tracked!",
-          icon: "success",
-          toast: true,
-          position: "top",
-          showConfirmButton: false,
-          timer: 3000,
-        });
-      } else {
-        db.create("TRACK_SAVED_LOCATION_WEATHER", true);
-        Swal.fire({
-          text: "Saved location would be tracked by default!",
-          icon: "info",
-          toast: true,
-          position: "top",
-          showConfirmButton: false,
-          timer: 3000,
-        });
-      }
-    } else {
-      if (db.get("TRACK_SAVED_LOCATION_WEATHER")) {
-        db.update("TRACK_SAVED_LOCATION_WEATHER", false);
-        Swal.fire({
-          text: "Saved location would not be tracked!",
-          icon: "warning",
-          toast: true,
-          position: "top",
-          showConfirmButton: false,
-          timer: 3000,
-        });
-      }
-    }
-  });
+		if ($toggleBtn.checked) {
+			//check if the value is in the database, then update it
+			if (db.get("TRACK_SAVED_LOCATION_WEATHER")) {
+				db.update("TRACK_SAVED_LOCATION_WEATHER", true);
+				Swal.fire({
+					text: "Saved location would be tracked!",
+					icon: "success",
+					toast: true,
+					position: "top",
+					showConfirmButton: false,
+					timer: 3000,
+				});
+			} else {
+				db.create("TRACK_SAVED_LOCATION_WEATHER", true);
+				Swal.fire({
+					text: "Saved location would be tracked by default!",
+					icon: "info",
+					toast: true,
+					position: "top",
+					showConfirmButton: false,
+					timer: 3000,
+				});
+			}
+		} else {
+			if (db.get("TRACK_SAVED_LOCATION_WEATHER")) {
+				db.update("TRACK_SAVED_LOCATION_WEATHER", false);
+				Swal.fire({
+					text: "Saved location would not be tracked!",
+					icon: "warning",
+					toast: true,
+					position: "top",
+					showConfirmButton: false,
+					timer: 3000,
+				});
+			}
+		}
+	});
 };
 
 export const checkTrackedLocation = () => {
-  let value = db.get("TRACK_SAVED_LOCATION_WEATHER");
-  console.log("value is " + value);
-  if (value === true) {
-    console.log("value is now" + value);
-
-    return true;
-  } else {
-    return false;
-  }
+	let value = db.get("TRACK_SAVED_LOCATION_WEATHER");
+	if(value === true) {
+		return true;
+	} else {
+		return false;
+	}
 };
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -6,140 +6,153 @@ import Spinner from "../components/spinner";
 import { db } from "../backend/app_backend";
 import * as settings from "./../backend/settings";
 const Settings = () => {
-  const navigateHome = () => {
-    navigate("./weather");
-  };
+	const navigateHome = () => {
+		navigate("./weather");
+	};
 
+	const [defaultLocation, setDefaultLocation] = useState(
+		settings.getDefaultLocation()
+	);
+	//database returns a string
+	let trackedLocation = db.get("TRACK_SAVED_LOCATION_WEATHER");
+	let trackedLocationLegit = trackedLocation == "true" ? true : false;
 
-  const [defaultLocation, setDefaultLocation] = useState(
-    settings.getDefaultLocation()
-  );
-  //database returns a string
-  let trackedLocation = db.get("TRACK_SAVED_LOCATION_WEATHER");
-  let trackedLocationLegit = (trackedLocation == "true") ? true : false;
- 
-  return (
-    <React.Fragment>
-      <Spinner />
-      <section className="container-fluid">
-        <section className="app-header d-flex justify-content-between my-3">
-          <div className="toggle-btn ">
-            <svg
-              height={"30"}
-              id="Layer_1"
-              version="1.1"
-              onClick={navigateHome}
-              viewBox="0 0 512 512"
-              width={"30"}
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <polygon points="352,128.4 319.7,96 160,256 160,256 160,256 319.7,416 352,383.6 224.7,256 " />
-            </svg>
-          </div>
-          <section className="city-locaton">
-            <h5 className="fw-bold fs-5 text-capitalize">change Settings</h5>
-          </section>
-          <div className="toggle-btn "></div>
-        </section>
-        <section className="d-flex align-items-start justify-content-center w-100">
-          <section className="settings">
-            <form
-              action=""
-              id="settingsForm"
-              onSubmit={(e) => {
-                settings.saveLocation(e);
-              }}
-            >
-              <label
-                htmlFor="defaultLocation "
-                className="brand-small-text py-3"
-              >
-                Update your default location
-              </label>
-              <input
-                type="text"
-                name="defaultLocation"
-                id="defaultLocation"
-                className="form-control p-3 my-1 brand-small-text "
-                value={defaultLocation}
-                placeholder={"Enter your default location to track.."}
-                onChange={(e) => {
-                  setDefaultLocation(e.target.value);
-                }}
-              />
-              <section className="my-2 d-md-flex align-items-center justify-content-md-center d-lg-block">
-                <Button
-                  text="save location"
-                  className="shadow brand-btn-3  my-5 text-light text-capitalize"
-                />
-              </section>
+	return (
+		<React.Fragment>
+			<Spinner />
+			<section className="container-fluid">
+				<section className="app-header d-flex justify-content-between my-3">
+					<div className="toggle-btn ">
+						<svg
+							height={"30"}
+							id="Layer_1"
+							version="1.1"
+							onClick={navigateHome}
+							viewBox="0 0 512 512"
+							width={"30"}
+							xmlns="http://www.w3.org/2000/svg">
+							<polygon points="352,128.4 319.7,96 160,256 160,256 160,256 319.7,416 352,383.6 224.7,256 " />
+						</svg>
+					</div>
+					<section className="city-locaton">
+						<h5 className="fw-bold fs-5 text-capitalize">change Settings</h5>
+					</section>
+					<div className="toggle-btn "></div>
+				</section>
+				<section className="d-flex align-items-start justify-content-center w-100">
+					<section className="settings">
+						<form
+							action=""
+							id="settingsForm"
+							onSubmit={(e) => {
+								settings.saveLocation(e);
+							}}>
+							<label
+								htmlFor="defaultLocation "
+								className="brand-small-text py-3">
+								Update your default location
+							</label>
+							<input
+								type="text"
+								name="defaultLocation"
+								id="defaultLocation"
+								className="form-control p-3 my-1 brand-small-text "
+								value={defaultLocation}
+								placeholder={"Enter your default location to track.."}
+								onChange={(e) => {
+									setDefaultLocation(e.target.value);
+								}}
+							/>
+							<section className="my-2 d-md-flex align-items-center justify-content-md-center d-lg-block">
+								<Button
+									text="save location"
+									className="shadow brand-btn-3  my-5 text-light text-capitalize"
+								/>
+							</section>
 
-              <label htmlFor="defaultWeatherUnit " className="text-capitalize py-2 my-2">select your default weather unit</label>
+							<label
+								htmlFor="defaultWeatherUnit "
+								className="text-capitalize py-2 my-2">
+								select your default weather unit
+							</label>
 
-              <div className="mb-3">
-               
-                <select className="form-select form-select my-2" name="" id="">
-                  <option defaultValue="SELECT" className="text-capitalize">Select Unit</option>
-                  <option defaultValue="" className="text-capitalize">New Delhi</option>
-                  <option defaultValue="" className="text-capitalize">Istanbul</option>
-                  <option defaultValue="" className="text-capitalize">Jakarta</option>
-                </select>
-              </div>
-              <br/>
+							<div className="mb-3">
+								<select className="form-select form-select my-2" name="" id="">
+									<option defaultValue="SELECT" className="text-capitalize">
+										Select Unit
+									</option>
+									<option defaultValue="" className="text-capitalize">
+										New Delhi
+									</option>
+									<option defaultValue="" className="text-capitalize">
+										Istanbul
+									</option>
+									<option defaultValue="" className="text-capitalize">
+										Jakarta
+									</option>
+								</select>
+							</div>
 
-              <hr className="horizontal-line py-3 w-75 m-auto " />
+							<section className="my-2 d-md-flex align-items-center justify-content-md-center d-lg-block">
+								<Button
+									text="save unit"
+									className="shadow brand-btn-3-secondary toggle-width-3 my-5 text-dark text-capitalize p-2"
+								/>
+							</section>
 
-              <section className="factory-settings my-3">
-                <label
-                  htmlFor="factory-settings-reset "
-                  className="text-capitalize"
-                >
-                  restore factory settings
-                </label>
-                <section className="d-md-flex align-items-center justify-content-center d-lg-block">
-                  <Button
-                    text="restore settings"
-                    className="shadow brand-btn-3-secondary toggle-width-3 my-5 text-dark text-capitalize p-2"
-                    onClick={settings.restoreFactorySettings}
-                  />
-                </section>
-                <p className="text-muted brand-small-text">
-                  Restoring factory settings removes all the data stored on this
-                  device. You would be taken to the setup screen.
-                </p>
-              </section>
+							<br />
 
-              <section className="form-check form-switch my-3">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  role="switch"
-                  id="flexSwitchCheckDefault"
-                  onClick={settings.trackSavedLocationWeather}
-                  defaultChecked={trackedLocationLegit}
-                />
-                <label
-                  className="form-check-label text-capitalize"
-                  htmlFor="flexSwitchCheckDefault"
-                >
-                  track saved location weather
-                </label>
-              </section>
-              <br />
-              <br />
-            </form>
-            <br />
-            <br />
-          </section>
-        </section>
+							<hr className="horizontal-line py-3 w-75 m-auto " />
 
-        <br />
-        <br />
+							<section className="factory-settings my-3">
+								<label
+									htmlFor="factory-settings-reset "
+									className="text-capitalize">
+									restore factory settings
+								</label>
+								<section className="d-md-flex align-items-center justify-content-center d-lg-block">
+									<Button
+										text="restore settings"
+										className="shadow brand-btn-3-secondary toggle-width-3 my-5 text-dark text-capitalize p-2"
+										onClick={settings.restoreFactorySettings}
+									/>
+								</section>
+								<p className="text-muted brand-small-text">
+									Restoring factory settings removes all the data stored on this
+									device. You would be taken to the setup screen.
+								</p>
+							</section>
 
-        <Footer />
-      </section>
-    </React.Fragment>
-  );
+							<section className="form-check form-switch my-3">
+								<input
+									className="form-check-input"
+									type="checkbox"
+									role="switch"
+									id="flexSwitchCheckDefault"
+									onClick={settings.trackSavedLocationWeather}
+									defaultChecked={trackedLocationLegit}
+								/>
+								<label
+									className="form-check-label text-capitalize"
+									htmlFor="flexSwitchCheckDefault">
+									track saved location weather
+								</label>
+							</section>
+							<br />
+							<br />
+						</form>
+						<br />
+						<br />
+					</section>
+				</section>
+
+				<br />
+				<br />
+
+				<Footer />
+			</section>
+		</React.Fragment>
+	);
 };
 
 export default Settings;

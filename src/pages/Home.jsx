ea7c81b306316e4fe84cde34a01867ae7e94c2ca
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-22 14:47:05 +0100

    Updated the weather unit settings

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -56,6 +56,7 @@ const Home = () => {
             db.create("HOME_PAGE_SEEN", true);
             db.create("USER_DEFAULT_LOCATION", $defaultLocation);
             db.create("TRACK_SAVED_LOCATION_WEATHER",false);
+            db.create("WEATHER_UNIT","celsius");
             navigate("weather");
           }
         });

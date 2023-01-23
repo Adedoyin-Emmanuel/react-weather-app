89b396160114da807d8dbc0ab069e344cb7bbd90
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-23 02:50:13 +0100

    Testing

--- src/components/utilityFooterComponet.jsx
+++ src/components/utilityFooterComponet.jsx
@@ -3,29 +3,30 @@ import jQuery from "jquery";
 import { useSwipeable } from "react-swipeable";
 
 const UtilityComponent = (props) => {
-    const closeUtilityComponent = ()=> {
-        jQuery(($) => {
-          $.noConflict();
-          $(".cmp").addClass("d-none");
-          $(".utility-component").removeClass("add-utility-component-height");
-        
-        });
-      }
-    
-    const swipe = direction =>{
-        console.log(`you swipped ${direction}`);
-    }
-  return (
-    <React.Fragment>
-     <useSwipeable onSwipe={swipe}>
-      <section className="utility-component align-items-center justify-content-around m-auto width-toggle-2" id="utilityComponent">
-      <div className="utility-notch my-2" onClick={closeUtilityComponent}></div>
+	const closeUtilityComponent = () => {
+		jQuery(($) => {
+			$.noConflict();
+			$(".cmp").addClass("d-none");
+			$(".utility-component").removeClass("add-utility-component-height");
+		});
+	};
 
-        {props.tags}
-      </section>
-      </useSwipeable>
-    </React.Fragment>
-  );
+	const swipe = (direction) => {
+		alert(`you swipped ${direction}`);
+	};
+	return (
+		<useSwipeable onSwipe={swipe} className="d-flex align-items-center justify-content-center">
+			<section
+				className="utility-component align-items-center justify-content-around m-auto width-toggle-2"
+				id="utilityComponent">
+				<div
+					className="utility-notch my-2"
+					onClick={closeUtilityComponent}></div>
+
+				{props.tags}
+			</section>
+		</useSwipeable>
+	);
 };
 
 export default UtilityComponent;

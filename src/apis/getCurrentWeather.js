5f0e2cacc2383899037b9098ac242f1d5f0236b1
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-23 17:41:54 +0100

    Added the weather rainy code status

--- src/apis/getCurrentWeather.js
+++ src/apis/getCurrentWeather.js
@@ -56,11 +56,16 @@ export const checkWeatherCode = code =>{
 	//check the result code states and allocate different icon svg depding on the weather code
 	if(code >= 200 && code != 300)
 	{
+		//Thunder weather status
 		weatherSvg = "Thunder";
 
 	}else if (code >= 300 && code != 400)
 	{
-		
+		//Drizzle weather status
+		weatherSvg = "Drizzle";
+	}else if (code >= 500 && code != 511 && code != 600)
+	{
+		//Rainy weather status
 	}
 }
 

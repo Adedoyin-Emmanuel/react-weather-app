d3f5bfcbf64de6ef3fabb78cc49316d4d07ae8a3
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-23 02:46:33 +0100

    Bug fix

--- src/components/utilityFooterComponet.jsx
+++ src/components/utilityFooterComponet.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import jQuery from "jquery";
-import Swipeable from "react-swipeable";
+import { useSwipeable } from "react-swipeable";
 
 const UtilityComponent = (props) => {
     const closeUtilityComponent = ()=> {
@@ -17,13 +17,13 @@ const UtilityComponent = (props) => {
     }
   return (
     <React.Fragment>
-      <Swipeable onSwipe={swipe}>
+     <useSwipeable onSwipe={swipe}>
       <section className="utility-component align-items-center justify-content-around m-auto width-toggle-2" id="utilityComponent">
       <div className="utility-notch my-2" onClick={closeUtilityComponent}></div>
 
         {props.tags}
       </section>
-      </Swipeable>
+      </useSwipeable>
     </React.Fragment>
   );
 };

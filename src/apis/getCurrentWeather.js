332a17ed82ea8c88e6c3a636389be2a5ea82ab02
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-27 02:59:32 +0100

    Bug fix

--- src/apis/getCurrentWeather.js
+++ src/apis/getCurrentWeather.js
@@ -154,7 +154,7 @@ export const updateReactDom = (result) => {
 		db.create("WEATHER_CODE", result.weather[0].id);
 		db.create("SUB_WEATHER_WIND_VALUE", `${result.wind.speed} m/s`);
 		db.create("SUB_WEATHER_HUMIDITY_VALUE", `${result.main.humidity} %`);
-		db.create("SUB_WEATHER_PRESSURE_VALUE", `${result.main.presssure} hPa`);
+		db.create("SUB_WEATHER_PRESSURE_VALUE", `${result.main.pressure} hPa`);
 	});
 };
 export const getCurrentWeather = (location) => {

0bb8cc4bec7acb71f0384c32f9f4d6fad94cff31
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-27 02:54:10 +0100

    Added a pressure value fallback for first time users

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -328,7 +328,7 @@ const WeatherApp = () => {
 						<p
 							className="rain-value fw-bold text-light brand-small-text  text-center py-1 m-0"
 							id="pressure-value">
-							100%
+							{db.get("SUB_WEATHER_PRESSURE_VALUE") || "100%"}
 						</p>
 						<p className="m-0 rain-text text-muted text-capitalize text-center brand-small-text-2 weather-text">
 							Pressure

3f7a4143f69f6777b5e39da11ad72255875cbd4c
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-27 20:35:25 +0100

    Modified the dashboard icon color

--- src/pages/ForecastWeather.jsx
+++ src/pages/ForecastWeather.jsx
@@ -119,15 +119,19 @@ const navigateToApp = () =>{
 						<h5 className="fw-bold fs-5 my-3">Lagos, 9ja</h5>
 					</section>
 					<div className="toggle-btn my-3">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width={"24px"}
-							height={"24px"}
-							viewBox="0 0 24 24"
-							onClick={showMainWeatherComponent}>
-							<path fill="none" d="M0 0h24v24H0V0z" />
-							<path d="M4 13h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1zm0 8h6c.55 0 1-.45 1-1v-4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1zm10 0h6c.55 0 1-.45 1-1v-8c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1zM13 4v4c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1z" />
-						</svg>
+          <svg
+						xmlns="http://www.w3.org/2000/svg"
+						width={"30px"}
+						height={"30px"}
+						viewBox="0 0 24 24"
+						className="d-block"
+						onClick={showMainWeatherComponent}>
+						<path fill="white" d="M0 0h24v24H0V0z" />
+						<path
+							fill="lightskyblue"
+							d="M4 13h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1zm0 8h6c.55 0 1-.45 1-1v-4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1zm10 0h6c.55 0 1-.45 1-1v-8c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1zM13 4v4c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1z"
+						/>
+					</svg>
 					</div>
 				</section>
 				<section className="my-4 next-week-component-container d-flex align-items-center justify-content-start flex-column">

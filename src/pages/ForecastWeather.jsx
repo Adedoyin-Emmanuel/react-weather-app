9324cd98df1e4bec61855e6f3b7cb9a6c3138e41
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-27 20:32:21 +0100

    Added the navigate To app function

--- src/pages/ForecastWeather.jsx
+++ src/pages/ForecastWeather.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import rainIcon from "./../assets/rain (1).png";
 import windIcon from "./../assets/windy.png";
 import humidity from "./../assets/humidity.png";
@@ -13,68 +13,28 @@ import { getCurrentDate } from "../inc/scripts/utilities";
 import * as formHandler from "./../apis/getCurrentWeather";
 
 const ForecastWeather = () => {
-  //holds the current component to insert into the utility footer component
+	//holds the current component to insert into the utility footer component
 	const [componentToInsert, setComponentToInsert] = useState("");
 
-  const addUtilityComponentHeight = () => {
-		//if the component is opened already, then close it
-		// if (db.get("UTILITY_CMP_DISPLAY") == "true") {
-		// 	closeUtilityComponent();
-		// } else {
-		// 	//else open the component
+	const addUtilityComponentHeight = () => {
 		jQuery(($) => {
 			$.noConflict();
 			$(".cmp").removeClass("d-none");
 			$(".utility-component").toggleClass("add-utility-component-height");
-			//db.create("UTILITY_CMP_DISPLAY", true);
 		});
-		//}
 	};
-
-	const closeUtilityComponent = () => {
-		//db.update("UTILITY_CMP_DISPLAY", false);
-
-		jQuery(($) => {
-			$.noConflict();
-			$(".cmp").addClass("d-none");
-			$(".utility-component").removeClass("add-utility-component-height");
-			setComponentToInsert("");
-		});
-
-		//update the search component
-		//setWeatherInput("");
-	};
-  const MainWeatherForecast = () =>{
-    return (
-      <section className="city-location">
-						<h5 className="fw-bold fs-5" id="weatherLocation">
-							{db.get("WEATHER_LOCATION") || "Lagos 9ja"}
-						</h5>
-						<p className="date-time text-muted brand-small-text text-capitalize">
-							{getCurrentDate()}
-						</p>
-					</section>
-    )
-  }
-
-  	//create the main weather component forecast tags
+const navigateToApp = () =>{
+  navigate("/weather");
+}
+	//create the main weather component forecast tags
 	const MainWeatherComponent = () => {
 		return (
-      <section className="cmp-container d-flex align-items-start flex-column">
-			<section className="cmp d-flex align-items-center justify-content-center flex-column my-5">
-				<section className="d-flex flex-row align-items-center justify-content-center d-none cmp cmp-1 my-5">
-					{<MainWeatherForecast/>}
-				</section>
-				<section className="d-flex align-items-center justify-content-center">
-					<Button
-						text="main weather forecast"
-						className="shadow brand-btn-2 toggle-width-3 my-5 "
-					
-					/>
-				</section>
+			<section className="d-flex align-items-center justify-content-center">
+				<Button
+					text="current weather forecast"
+					className="shadow brand-btn-2 toggle-width-3 my-5 "
+				/>
 			</section>
-      </section>
-      
 		);
 	};
 
@@ -85,66 +45,74 @@ const ForecastWeather = () => {
 		setComponentToInsert(<MainWeatherComponent />);
 	};
 
+	const navigateHome = () => {
+		navigate("/weather");
+	};
+	const weekData = [
+		{
+			day: ["today"],
+			firstUnit: 13,
+			secondUnit: 22,
+			icon: rainIcon,
+		},
 
-  const navigateHome = ()=>{
-    navigate("/weather");
-  }
-  const weekData = [
-    {
-      day: ["today"],
-      firstUnit: 13,
-      secondUnit: 22,
-      icon: rainIcon,
-    },
+		{
+			day: ["today"],
+			firstUnit: 13,
+			secondUnit: 22,
+			icon: windIcon,
+		},
 
-    {
-      day: ["today"],
-      firstUnit: 13,
-      secondUnit: 22,
-      icon: windIcon,
-    },
+		{
+			day: ["today"],
+			firstUnit: 13,
+			secondUnit: 22,
+			icon: humidity,
+		},
 
-    {
-      day: ["today"],
-      firstUnit: 13,
-      secondUnit: 22,
-      icon: humidity,
-    },
+		{
+			day: ["today"],
+			firstUnit: 13,
+			secondUnit: 22,
+			icon: rainIcon,
+		},
 
-    {
-      day: ["today"],
-      firstUnit: 13,
-      secondUnit: 22,
-      icon: rainIcon,
-    },
+		{
+			day: ["today"],
+			firstUnit: 13,
+			secondUnit: 22,
+			icon: rainIcon,
+		},
+	];
 
-    {
-      day: ["today"],
-      firstUnit: 13,
-      secondUnit: 22,
-      icon: rainIcon,
-    }
-  ];
+	const uiData = weekData.map((data, index) => {
+		return (
+			<NextWeekComponent
+				key={index}
+				day={data.day}
+				firstUnit={data.firstUnit}
+				secondUnit={data.secondUnit}
+				icon={data.icon}
+			/>
+		);
+	});
 
-  const uiData = weekData.map((data, index) => {
-    return (
-      <NextWeekComponent
-        key={index}
-        day={data.day}
-        firstUnit={data.firstUnit}
-        secondUnit={data.secondUnit}
-        icon={data.icon}
-      />
-    );
-  });
-
-  return (
-    <React.Fragment>
-      <Spinner/>
-      <section className="container-fluid">
-      <section className="app-header d-flex justify-content-between">
+	return (
+		<React.Fragment>
+			<Spinner />
+			<section className="container-fluid">
+				<section className="app-header d-flex justify-content-between">
 					<div className="toggle-btn my-3">
-         <svg height={"30"} id="Layer_1" version="1.1" onClick={navigateHome}viewBox="0 0 512 512" width={"30"} xmlns="http://www.w3.org/2000/svg" ><polygon points="352,128.4 319.7,96 160,256 160,256 160,256 319.7,416 352,383.6 224.7,256 "/></svg>
+						<svg
+							height={"30"}
+							id="Layer_1"
+							version="1.1"
+							onClick={navigateHome}
+							viewBox="0 0 512 512"
+							width={"30"}
+							xmlns="http://www.w3.org/2000/svg">
+							<polygon points="352,128.4 319.7,96 160,256 160,256 160,256 319.7,416 352,383.6 224.7,256 " />
+						</svg>
 					</div>
 					<section className="city-locaton">
 						<h5 className="fw-bold fs-5 my-3">Lagos, 9ja</h5>
@@ -155,21 +123,22 @@ const ForecastWeather = () => {
 							width={"24px"}
 							height={"24px"}
 							viewBox="0 0 24 24"
-              onClick={showMainWeatherComponent}
-              >
+							onClick={showMainWeatherComponent}>
 							<path fill="none" d="M0 0h24v24H0V0z" />
 							<path d="M4 13h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1zm0 8h6c.55 0 1-.45 1-1v-4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1zm10 0h6c.55 0 1-.45 1-1v-8c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1zM13 4v4c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1z" />
 						</svg>
 					</div>
 				</section>
-        <section className="my-4 next-week-component-container d-flex align-items-center justify-content-start flex-column">
-          {uiData}
-        </section>
-        <Footer utilityTags={componentToInsert}/>
-        <br/><br/><br/>
-      </section>
-    </React.Fragment>
-  );
+				<section className="my-4 next-week-component-container d-flex align-items-center justify-content-start flex-column">
+					{uiData}
+				</section>
+				<Footer utilityTags={componentToInsert} />
+				<br />
+				<br />
+				<br />
+			</section>
+		</React.Fragment>
+	);
 };
 
 export default ForecastWeather;

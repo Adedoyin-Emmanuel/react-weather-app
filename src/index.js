297f29405a699b763777970e784b6e26cefa2567
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-27 21:02:52 +0100

    Updated the service worker filenmae

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerFile from "./serviceWorkerRegistration";
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -12,7 +11,7 @@ root.render(
 );
 
 
-serviceWorkerFile.register();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

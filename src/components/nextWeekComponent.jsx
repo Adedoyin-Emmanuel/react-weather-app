d9327481f3e4d33f59cbd2cec225dfc0f5053d3f
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-27 21:37:03 +0100

    Added more styling to the fforecast page

--- src/components/nextWeekComponent.jsx
+++ src/components/nextWeekComponent.jsx
@@ -3,20 +3,20 @@ import React from "react";
 const NextWeekComponent = (props) => {
 	return (
 		<React.Fragment>
-			<section className="w-100 next-week-component d-flex align-items-start justify-content-between ">
-				<section className="today-section d-flex align-items-start justify-content-start flex-row">
-					<p className="text-start fw-bold text-capitalize brand-small-text d-block">{props.day}</p>
+			<section className="w-100 next-week-component d-flex align-items-start justify-content-between brand-tertiary-color p-4 rounded-3 shadow">
+				<section className="today-section d-flex align-items-start justify-content-start flex-row text-light">
+					<p className="text-start fw-bold text-capitalize brand-small-text d-block m-0">{props.day}</p>
 				</section>
-				<section className="d-flex x align-items-center justify-content-between">
-						<p className="text-muted brand-small-textd-block px-2">
+				<section className="d-flex align-items-center justify-content-between">
+						<p className="text-muted brand-small-textd-block px-2 text-light m-0">
 							{props.firstUnit}<sup>o</sup>
 						</p>
-						<div className="next-week-notch d-block"></div>
-						<p className="fw-bold brand-small-text d-block px-2">
+						<div className="next-week-notch d-block mt-3"></div>
+						<p className="fw-bold brand-small-text d-block px-2 text-light m-0">
 							{props.secondUnit}<sup>o</sup>
 						</p>
 				</section>
-					<section className="next-week-weather-x align-items-center justify-content-betweenicon">
+					<section className="next-week-weather-x align-items-center justify-content-between icon">
 							<img src={props.icon} height={"20"} width = {"20"} alt={"weather-icons"}/>
 					</section>
 				

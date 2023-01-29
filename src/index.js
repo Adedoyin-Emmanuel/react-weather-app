5e504bc520576d1661b464a11a6fee82a41cb96c
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-29 20:39:34 +0100

    Added the service worker file

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
 );
 
 
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register("./service-worker.js");
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

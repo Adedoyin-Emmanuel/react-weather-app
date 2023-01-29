d2ebaedb0a888019e26717271d99b075e18309cc
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-29 02:10:47 +0100

    Added the title prop to the forecastWeatherComponent

--- src/components/forecastWeatherComponent.jsx
+++ src/components/forecastWeatherComponent.jsx
@@ -4,11 +4,11 @@ const ForecastDailyWeatherComponent = props =>{
    
     return (
        <React.Fragment>
-            <section className="future-weather-container d-flex align-items-center justify-content-center flex-column brand-bg-white px-4  rounded-3 shadow-sm mx-2 my-3">
+            <section className="future-weather-container d-flex align-items-center justify-content-center flex-column brand-bg-white px-4  rounded-3 shadow-sm mx-2 my-3" title={props.title}>
                 <section className="weather-wrapper d-flex flex-column align-items-center justify-content-center">
                     <p className="brand-small-text-2  py-1 text-center m-0">{props.time}</p>
                     <section className="weather-icon-section py-1">
-                            <img src ={props.icon} height={"30"} width={"30"}/>
+                            <img src ={props.icon} height={"40"} width={"40"}/>
                     </section>
                     <p className="brand-small-text fw-bold text-center m-0"> {props.weatherUnit}<sup>o</sup></p>
                 </section>

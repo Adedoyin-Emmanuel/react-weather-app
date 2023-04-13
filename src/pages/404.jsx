ccadafb092e0f252e3ec431b7109b06f695614f5
Thenujan Sandramohan <sthenujan2002@gmail.com>
2023-04-13 15:08:29 +0530

    Center 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -10,15 +10,15 @@ const NotFound = ()=>{
     return (
         <React.Fragment>
             <Spinner/>
-            <section className="container-fluid">
-                    <h2 className="text-capitalize my-3 fs-3 fw-bold">not found!</h2>
+                <section className="container-fluid d-flex flex-column align-items-center justify-content-center" style={{minHeight:"100vh"}}>
+                        <h2 className="text-capitalize my-3 fs-3 fw-bold text-center">not found!</h2>
 
-                    <p className="text-muted text-capitalize">the page requested for was not found!</p>
+                        <p className="text-muted text-capitalize text-center">the page requested for was not found!</p>
 
-                    <section className="d-flex align-items-center justify-content-center">
-                        <Button text="Home" className="brand-btn m-auto my-5 width-toggle" onClick={returnHome}/>
-                    </section>
-            </section>
+                        <section className="d-flex align-items-center justify-content-center">
+                            <Button text="Home" className="brand-btn m-auto my-5 width-toggle" onClick={returnHome}/>
+                        </section>
+                </section>
         </React.Fragment>
     )
 }

9ad42674ad4c8c031643fe895ba7326cb390ae92
Okeybukks <achebepeter94@gmail.com>
2023-07-16 21:59:47 +0100

    edit deliver script

--- scripts/deliver-for-development.sh
+++ scripts/deliver-for-development.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env sh
 
-echo 'The following "npm" command runs your Node.js/React application in'
-echo 'development mode and makes the application available for web browsing.'
-echo 'The "npm start" command has a trailing ampersand so that the command runs'
-echo 'as a background process (i.e. asynchronously). Otherwise, this command'
-echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
-echo 'is followed by another command that retrieves the process ID (PID) value'
-echo 'of the previously run process (i.e. "npm start") and writes this value to'
-echo 'the file ".pidfile".'
-
-npm start &
-echo $! > .pidfile
+npm start 
 
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'

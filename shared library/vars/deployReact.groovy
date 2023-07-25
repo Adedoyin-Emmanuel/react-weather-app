2d09002f132332a353c569e2996e02a96733bbcb
Okeybukks <achebepeter94@gmail.com>
2023-07-25 14:42:35 +0100

    test

--- shared library/vars/deployReact.groovy
+++ shared library/vars/deployReact.groovy
@@ -2,7 +2,6 @@ def call(String environment){
 
     sh "rm -rf /var/www/jenkins-weather-app-${environment}"
     copyArtifacts filter: 'build/**', fingerprintArtifacts: true, projectName: '${JOB_BASE_NAME}', selector: specific ('${BUILD_NUMBER}')
-    sh "ls build"
-    // sh "cp -r ${env.WORKSPACE}/build /var/www/jenkins-weather-app-${environment}"
-    // sh "ls /var/www/jenkins-weather-app-${environment}"
+    sh "cp -r build /var/www/jenkins-weather-app-${environment}"
+    sh "ls /var/www/jenkins-weather-app-${environment}"
 }

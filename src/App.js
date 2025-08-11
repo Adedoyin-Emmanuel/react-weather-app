c6c7fae72f00537c491c829a09425570d080f77c
kishoreav14 <165627227+kishoreav14@users.noreply.github.com>
2025-08-11 19:35:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { db } from "./backend/app_backend";
 import "./autoload";
 
 function App() {
+  console.log("Deployed Successfully")
   let homePageSeen = db.get("HOME_PAGE_SEEN");
   let DEFAULT_ROUTE_PAGE;
   homePageSeen
